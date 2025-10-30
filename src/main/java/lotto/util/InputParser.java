8b3c6def6b5dc59765f6aee817973f9c10a4dc00
LeeJaeHyeok97 <hazardous10@naver.com>
2025-10-30 13:11:42 +0900

    feat: 당첨 번호 구분자가 맨 앞이나 맨 뒤에 입력된 경우 예외 처리

--- src/main/java/lotto/util/InputParser.java
+++ src/main/java/lotto/util/InputParser.java
@@ -1,36 +1,39 @@
 package lotto.util;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class InputParser {
 
     public static final String DELIMITER = ",";
     public static final String CONTINUOUS_DELIMITER_REGEX = ",{2,}";
-    public static final Pattern CONTINUOUS_DELIMITER_PATTERN = Pattern.compile(CONTINUOUS_DELIMITER_REGEX);
     public static final String BLANK = " ";
+    public static final String STARTS_OR_ENDS_WITH_DELIMITER_REGEX = "^,|,$";
 
     public static String[] winningNumberParse(String input) {
-        Matcher matcher = CONTINUOUS_DELIMITER_PATTERN.matcher(input);
-        validate(input, matcher);
+        validate(input);
 
         return input.split(DELIMITER);
     }
 
-    private static void validate(String input, Matcher matcher) {
-        validateContinuousDelimiter(matcher);
+    private static void validate(String input) {
+        validateContinuousDelimiter(input);
+        validateStartsOrEndsWithDelimiter(input);
         validateContainsBlank(input);
+    }
+
+    private static void validateStartsOrEndsWithDelimiter(String input) {
+        if (input.matches(STARTS_OR_ENDS_WITH_DELIMITER_REGEX)) {
+            throw new IllegalArgumentException("[ERROR] 구분자가 맨 앞 또는 맨 뒤에 올 수 없습니다.");
+        }
     }
 
     private static void validateContainsBlank(String input) {
         if (input.contains(BLANK)) {
-            throw new IllegalArgumentException("[ERROR]당첨 번호는 공백일 수 없습니다.");
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 공백일 수 없습니다.");
         }
     }
 
-    private static void validateContinuousDelimiter(Matcher matcher) {
-        if (matcher.find()) {
-            throw new IllegalArgumentException("[ERROR]당첨 번호 구분자를 연속으로 입력할 수 없습니다.");
+    private static void validateContinuousDelimiter(String input) {
+        if (input.matches(CONTINUOUS_DELIMITER_REGEX)) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호 구분자를 연속으로 입력할 수 없습니다.");
         }
     }
 }
