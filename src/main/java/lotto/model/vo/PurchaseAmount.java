6380054c745104a40ffd6819fc9f55c163ff209d
LeeJaeHyeok97 <hazardous10@naver.com>
2025-10-30 14:05:11 +0900

    feat: PurchaseAmount 도메인 추가

---
+++ src/main/java/lotto/model/vo/PurchaseAmount.java
@@ -0,0 +1,10 @@
+package lotto.model.vo;
+
+public class PurchaseAmount {
+
+    private final Integer amount;
+
+    public PurchaseAmount(Integer amount) {
+        this.amount = amount;
+    }
+}
