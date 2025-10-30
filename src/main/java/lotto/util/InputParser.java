2a5c7468cfa47b9cdc7718056ad43983f25091d7
LeeJaeHyeok97 <hazardous10@naver.com>
2025-10-30 12:56:20 +0900

    feat: 당첨 번호 입력을 누락한 경우 예외 처리

--- src/main/java/lotto/util/InputParser.java
+++ src/main/java/lotto/util/InputParser.java
@@ -8,17 +8,29 @@ public class InputParser {
     public static final String DELIMITER = ",";
     public static final String CONTINUOUS_DELIMITER_REGEX = ",{2,}";
     public static final Pattern CONTINUOUS_DELIMITER_PATTERN = Pattern.compile(CONTINUOUS_DELIMITER_REGEX);
+    public static final String BLANK = " ";
 
     public static String[] winningNumberParse(String input) {
         Matcher matcher = CONTINUOUS_DELIMITER_PATTERN.matcher(input);
-        validateContinuousDelimiter(matcher);
+        validate(input, matcher);
 
         return input.split(DELIMITER);
     }
 
+    private static void validate(String input, Matcher matcher) {
+        validateContinuousDelimiter(matcher);
+        validateContainsBlank(input);
+    }
+
+    private static void validateContainsBlank(String input) {
+        if (input.contains(BLANK)) {
+            throw new IllegalArgumentException("[ERROR]당첨 번호는 공백일 수 없습니다.");
+        }
+    }
+
     private static void validateContinuousDelimiter(Matcher matcher) {
         if (matcher.find()) {
-            throw new IllegalArgumentException("당첨 번호 구분자를 연속으로 입력할 수 없습니다.");
+            throw new IllegalArgumentException("[ERROR]당첨 번호 구분자를 연속으로 입력할 수 없습니다.");
         }
     }
 }
