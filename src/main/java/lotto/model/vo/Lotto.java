ed3258315b984ef15739f10ae475e8aa1d2c8c7b
LeeJaeHyeok97 <hazardous10@naver.com>
2025-11-01 01:56:36 +0900

    feat: 발급한 로또를 오름차순으로 정렬하는 기능 추가

--- src/main/java/lotto/model/vo/Lotto.java
+++ src/main/java/lotto/model/vo/Lotto.java
@@ -1,8 +1,11 @@
 package lotto.model.vo;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class Lotto {
+
+    public static final int LOTTO_NUMBER_COUNT = 6;
 
     private final List<Integer> numbers;
 
@@ -11,13 +14,23 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public static Lotto of(List<Integer> numbers) {
+        return new Lotto(numbers);
+    }
+
+    public static Lotto issue(List<Integer> numbers) {
+        sortNumbers(numbers);
+
+        return Lotto.of(numbers);
+    }
+
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_NUMBER_COUNT) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    public static Lotto issue(List<Integer> numbers) {
-        return new Lotto(numbers);
+    private static void sortNumbers(List<Integer> numbers) {
+        numbers.sort(Comparator.naturalOrder());
     }
 }
