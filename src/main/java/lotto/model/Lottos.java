cbaee85f34d5627d2872cd9e7cd7150e2e96b554
LeeJaeHyeok97 <hazardous10@naver.com>
2025-11-01 13:23:27 +0900

    feat: 구매한 수량 만큼 로또를 발행하는 기능 구현

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import java.util.ArrayList;
 import java.util.List;
 import lotto.model.vo.Lotto;
 
@@ -9,5 +10,28 @@ public class Lottos {
 
     public Lottos(List<Lotto> values) {
         this.values = values;
+    }
+
+    public static Lottos of(List<Lotto> values) {
+        return new Lottos(values);
+    }
+
+    public Lottos generateByQuantity(int quantity, LottoNumbersGenerator lottoNumbersGenerator) {
+        List<Lotto> values = new ArrayList<>();
+
+        while (quantity > 0) {
+            values.add(getIssuedLotto(lottoNumbersGenerator));
+            quantity -= 1;
+        }
+
+        return Lottos.of(values);
+    }
+
+    private Lotto getIssuedLotto(LottoNumbersGenerator lottoNumbersGenerator) {
+        return Lotto.issue(getGenerateLottoNumbers(lottoNumbersGenerator));
+    }
+
+    private List<Integer> getGenerateLottoNumbers(LottoNumbersGenerator lottoNumbersGenerator) {
+        return lottoNumbersGenerator.generateLottoNumbers();
     }
 }
