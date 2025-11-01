a6a629946ecfb9691de1c82271619fce9622eaae
LeeJaeHyeok97 <hazardous10@naver.com>
2025-11-02 00:23:22 +0900

    feat: 당첨된 통계를 집계하는 기능 추가

--- src/main/java/lotto/model/WinningRank.java
+++ src/main/java/lotto/model/WinningRank.java
@@ -1,20 +1,35 @@
 package lotto.model;
 
+import java.util.Arrays;
+
 public enum WinningRank {
 
+    NONE(0, false, 0),
     FIFTH_PRIZE(3, false, 5000),
     FOURTH_PRIZE(4, false, 50000),
     THIRD_PRIZE(5, false, 1500000),
     SECOND_PRIZE(5, true, 30000000),
     FIRST_PRIZE(6, false, 2000000000);
 
-    private int matchCount;
-    private boolean bonusMatch;
-    private int prize;
+    private final int matchCount;
+    private final boolean bonusMatch;
+    private final int prize;
 
     WinningRank(int matchCount, boolean bonusMatch, int prize) {
         this.matchCount = matchCount;
         this.bonusMatch = bonusMatch;
         this.prize = prize;
+    }
+
+    public static WinningRank findByMatchCountAndBonusMatch(int count, boolean bonusMatch) {
+        return Arrays.stream(values())
+                .filter(rank -> rank.matchCount == count)
+                .filter(rank -> rank.bonusMatch == bonusMatch)
+                .findFirst()
+                .orElse(NONE);
+    }
+
+    public int getPrize() {
+        return prize;
     }
 }
