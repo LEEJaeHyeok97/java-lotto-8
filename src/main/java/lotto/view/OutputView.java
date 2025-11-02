87c3791f8c4a37c3e00036d8eceaf2963187246c
LeeJaeHyeok97 <hazardous10@naver.com>
2025-11-02 12:44:25 +0900

    feat: 구매한 로또 개수를 출력하는 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,6 +5,12 @@ import lotto.model.vo.Lotto;
 
 public class OutputView {
 
+    public static final String PURCHASE_COUNT_MESSAGE = "%d개를 구매했습니다.";
+
+    public void printPurchaseCount(int count) {
+        System.out.printf(PURCHASE_COUNT_MESSAGE, count);
+    }
+
     public void printIssuedLottos(Lottos lottos) {
         for (Lotto lotto : lottos) {
             System.out.println(lotto.getNumbers());
